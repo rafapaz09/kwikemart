2accd8fb9dd95c1f5acb7e53a9bc033f1a3a320e
Rafael José Paz Sandoval <rafaelpazsv@gmail.com>
2019-04-05 21:57:26 -0600

    Installing Newtonsoft Json Nuget Installing Swashbuckle for documentation Configuring Swashbuckle

--- Kwikemart_API/Kwikemart_API/Startup.cs
+++ Kwikemart_API/Kwikemart_API/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -10,21 +11,43 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Kwikemart_API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration Configuration { get; set; }
+        public IHostingEnvironment env { get; set; }
+
+        public Startup(IConfiguration configuration,IHostingEnvironment Env)
         {
             Configuration = configuration;
+            env = Env;
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddMvcCore()
+                .AddAuthorization()
+                .AddJsonFormatters();
+
+            var builder = new ConfigurationBuilder()
+                            .SetBasePath(Directory.GetCurrentDirectory())
+                           .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+            Configuration = builder.Build();
+
+            string apiName = Configuration["ApiName"].ToString();
+            string version = Configuration["ApiVersion"].ToString();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(version, new Info { Title = apiName, Version = version });
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -40,7 +63,17 @@ namespace Kwikemart_API
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            app.UseSwagger(c =>
+            {
+                c.RouteTemplate = "docs/{documentName}/docs.json";
+            });
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/docs/v1/docs.json", "My API v1");
+            });
+
+            
             app.UseMvc();
         }
     }
