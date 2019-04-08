8a0d48cc8e084132de785e4b61eaa6c929ae8e7c
Rafael José Paz Sandoval <rafaelpazsv@gmail.com>
2019-04-07 23:55:33 -0600

    Adding JWT Authentication on startup, and secret variable

--- Kwikemart_API/Kwikemart_API/Startup.cs
+++ Kwikemart_API/Kwikemart_API/Startup.cs
@@ -1,18 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Kwikemart_API.Filters;
+﻿using System.IO;
+using System.Text;
+using Kwikemart_API.Helpers;
+using Kwikemart_API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace Kwikemart_API
@@ -45,16 +41,44 @@ namespace Kwikemart_API
             string apiName = Configuration["ApiName"].ToString();
             string version = Configuration["ApiVersion"].ToString();
 
+
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+            
+            //JWT AUTHENTICATION KEY GENERATOR
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+           {
+               x.RequireHttpsMetadata = false;
+               x.SaveToken = true;
+               x.TokenValidationParameters = new TokenValidationParameters
+               {
+                   ValidateIssuerSigningKey = true,
+                   IssuerSigningKey = new SymmetricSecurityKey(key),
+                   ValidateIssuer = false,
+                   ValidateAudience = false,
+                   ValidateLifetime = true
+               };
+           });
+
+            services.AddScoped<IUserService,UserService>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<IOrderService, OrderService>();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc(version, new Info { Title = apiName, Version = version });
             });
 
-            services.AddMvc(options =>
-            {
-                options.Filters.Add(typeof(ActionFilter));
-            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -76,9 +100,10 @@ namespace Kwikemart_API
 
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/docs/v1/docs.json", "My API v1");
+                c.SwaggerEndpoint("/docs/v1/docs.json", "KwikEMart API");
             });
 
+            app.UseAuthentication();
             app.UseMvc();
         }
 
